4084fb9a3bb4c4c270bb2e024857f84e5b9f0ffa
Dasha <dashamarrow@yandex.ru>
2023-06-24 15:59:59 +0300

    12.0. Написать тесты на StartUI. [#785  [#785]]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -29,7 +29,7 @@ public class StartUI {
                 new DeleteAction(),
                 new FindByIdAction(),
                 new FindByNameAction(),
-                new ExitAction()
+                new Exit()
         };
         new StartUI().init(input, tracker, actions);
     }
