329c19aeec04f27eb0470017068689029f9c2c06
Dasha <dashamarrow@yandex.ru>
2023-06-26 16:49:59 +0300

    0.4. Пользовательские исключения. [#219367  [#219367]]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,27 @@
+package ru.job4j.ex;
+
+import java.util.Objects;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (Objects.equals(value[i], key)) {
+                rsl = i;
+            }
+            if (rsl == -1) {
+                throw new ElementNotFoundException("Нет элемента");
+            }
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] massive = {"one", "two", "three"};
+        try {
+            indexOf(massive, "four");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
