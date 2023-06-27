2aa25565660025b01576508ae8f194f1547bbefa
Dasha <dashamarrow@yandex.ru>
2023-06-27 12:32:47 +0300

    0.4. Пользовательские исключения. [#219367  [#219367 #425647]]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -8,10 +8,11 @@ public class FindEl {
         for (int i = 0; i < value.length; i++) {
             if (Objects.equals(value[i], key)) {
                 rsl = i;
+                break;
             }
-            if (rsl == -1) {
-                throw new ElementNotFoundException("Нет элемента");
-            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Нет элемента");
         }
         return rsl;
     }
